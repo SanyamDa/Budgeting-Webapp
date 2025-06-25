bf4bc9b639804e68e2d66bbf1cbe8edc10f9fdcb
SanyamDa <sanyambkk@gmail.com>
2025-06-25 10:03:45 +0700

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h1>💸 My Budgeting App</h1>
-      <p>Welcome! Let’s help you track your money.</p>
-    </div>
-  )
-}
